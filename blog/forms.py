dffeafac0badccecc074aea73efd733050b80199
Andres Veintimilla <andveti@gmail>
2023-08-07 19:09:20 -0500

    Creación views, urls y rest api

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Publicacion
+from .models import Comentario
+
+class PublicacionForm(forms.ModelForm):
+    class Meta:
+        model = Publicacion
+        fields = ['titulo', 'contenido', 'autor']
+class ComentarioForm(forms.ModelForm):
+    class Meta:
+        model = Comentario
+        fields = ('autor', 'contenido',)
